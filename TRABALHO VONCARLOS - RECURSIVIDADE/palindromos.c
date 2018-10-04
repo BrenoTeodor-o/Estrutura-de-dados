d8225c79da444f9b1280d0d36f281a6887bcf696
BrenoTeodor-o <teodoro.breno@outlook.com.br>
2018-10-03 22:25:34 -0300

    agora vai mesmo

--- TRABALHO VONCARLOS - RECURSIVIDADE/palindromos.c
+++ TRABALHO VONCARLOS - RECURSIVIDADE/palindromos.c
@@ -4,13 +4,13 @@
 
 char palindromo (char word[999])
 {
-    int j,flag=0; //inicia  flag com 0;
+    int j,flag=0;
 
 	printf("Contrario: ");
 	for(j=strlen(word); j>=0; j--){
 			printf("%c",word[j]);
 			if((word[strlen(word)-1]==word[0]) && (word[strlen(word)-2]==word[1]))
-				flag++; //Se a condição for confirmada, flag passa a valer 1;
+				flag++;
 	}
 	printf("\n\n");
 	if(flag!=0)
@@ -22,19 +22,23 @@ char palindromo (char word[999])
 
 char Verifica(char str[])
 {
-   if((str[strlen(str)-1]==str[0]) && (str[strlen(str)-2]==str[1])){
-        printf("EH PALINDRIMO");
+
+    if((str[strlen(str)-1]==str[0]) && (str[strlen(str)-2]==str[1])){
+        printf("EH PALINDRIMO\n");
         return 1;
-   } else {
+    } else {
       if (str[0] != str[strlen(str)-1]){
-            printf("NAO EH PALINDRIMO");
+            printf("NAO EH PALINDRIMO\n");
             return 0;
       }
-      return Verifica( str + 1);
-   }
+    }
+    return Verifica( str + 1);
 }
 
-void main(){
+
+int main(){
     palindromo("socorram-me subi no onibus em marrocos");
     Verifica("socorram-me subi no onibus em marrocos");
+
+    return 0;
 }
