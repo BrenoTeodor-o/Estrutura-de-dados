3245e0daca7067073e83dffc3bbbb75f8645c606
BrenoTeodor-o <teodoro.breno@outlook.com.br>
2018-10-06 10:01:30 -0300

    fatorial.c without errors

--- TRABALHO VONCARLOS - RECURSIVIDADE/fatorial.c
+++ TRABALHO VONCARLOS - RECURSIVIDADE/fatorial.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int fat_iterativo(int n=0)
+int fat_iterativo(int n)
 {
-int fat=0;
-printf("Insira um valor para o qual deseja calcular seu fatorial: ");
-scanf("%d", &n);
+      n = 0;
+      int fat=0;
+      printf("Insira um valor para o qual deseja calcular seu fatorial: ");
+      scanf("%d", &n);
 
-for(fat = 1; n > 1; n = n - 1)
-fat = fat * n;
+      for(fat = 1; n > 1; n = n - 1)
+      fat = fat * n;
 
-printf("\nFatorial calculado: %d", fat);
+      printf("\nFatorial calculado: %d", fat);
 
 }
-int fat (n) {
-//Fun��o que calcula, recursivamente, o fatorial de n.
-if ((n==1) || (n==0)) return 1;
-   else
-      return fat(n-1)*n;
+int fat (int n) {
+      //Fun��o que calcula, recursivamente, o fatorial de n.
+      if ((n==1) || (n==0)) return 1;
+      else
+            return fat(n-1)*n;
 }
 
 
@@ -29,9 +30,9 @@ fat_iterativo(12);
 
 //Declara��o de n, o n�mero informado que gera o fatorial recursivo.
 int n;
-printf("\nInforme o n�mero que deseja obter o fatorial:\n");
+printf("\nInforme o numero que deseja obter o fatorial:\n");
 scanf("%d",&n);
-printf("O fatorial de %d � %d \n",n,fat (n));
+printf("O fatorial de %d eh %d \n",n,fat (n));
 
 return 0;
 }
